23a836211c7eb74c282d25d0974714fe37959931
Austin Riba <ariba@lcogt.net>
2015-09-02 13:51:11 -0700

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ LCOGT standards"""
 
 setup(
     name='lcogt-logging',
-    version='0.1.1',
+    version='0.2.0',
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     author='Austin Riba',
